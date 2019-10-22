fef26139b04d68ee3f20bc1e7bccb789b6c6d5bb
HarshalEzest <31441559+HarshalE-zest@users.noreply.github.com>
2019-10-22 17:04:20 +0530

    Added SOS pop up

---
+++ Enrich_SMA/Enrich_SMA/Modules/Appointment/SOS/SOSPopUpVC.swift
@@ -0,0 +1,47 @@
+//
+//  SOSPopUpVC.swift
+//  Enrich_SMA
+//
+//  Created by Harshal Patil on 22/10/19.
+//  Copyright © 2019 e-zest. All rights reserved.
+//
+
+import UIKit
+
+class SOSPopUpVC: UIViewController {
+
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var lblUserName: UILabel!
+    @IBOutlet weak var lblLevel: UILabel!
+    @IBOutlet weak var btnMobileNo: UIButton!
+    @IBOutlet weak var lblAddress: UILabel!
+    @IBOutlet weak var txtfMessage: UITextField!
+    @IBOutlet weak var btnOk: UIButton!
+    
+    var viewDismissBlock: ((Bool) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func actionMobileNo(_ sender: UIButton) {
+    }
+    
+    @IBAction func actionOK(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        viewDismissBlock!(true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Enrich_SMA/Enrich_SMA/Modules/More/MoreModuleVC.swift
+++ Enrich_SMA/Enrich_SMA/Modules/More/MoreModuleVC.swift
@@ -6,7 +6,7 @@
 import UIKit
 
 enum ProfileCellIdentifiers: String {
-
+    
     // Dashboard
     case myProfile = "MyProfile"
     case employees = "Employees"
@@ -15,7 +15,7 @@ enum ProfileCellIdentifiers: String {
     case audits = "Audits"
     case notifications = "Notifications"
     case logout = "Logout"
-
+    
 }
 
 protocol MoreModuleDisplayLogic: class {
@@ -25,31 +25,31 @@ protocol MoreModuleDisplayLogic: class {
 }
 
 class MoreModuleVC: UIViewController, MoreModuleDisplayLogic {
-
+    
     var interactor: MoreModuleBusinessLogic?
-
+    
     @IBOutlet weak private var tableView: UITableView!
-
+    
     var profileDashboardIdentifiers: [ProfileCellIdentifiers] = [.employees,
-                                                                .inventory,
-                                                                .stores,
-                                                                .audits,
-                                                                .notifications,
-                                                                .logout]
-
+                                                                 .inventory,
+                                                                 .stores,
+                                                                 .audits,
+                                                                 .notifications,
+                                                                 .logout]
+    
     // MARK: Object lifecycle
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         setup()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
-
+    
     // MARK: Setup
-
+    
     private func setup() {
         let viewController = self
         let interactor = MoreModuleInteractor()
@@ -57,19 +57,19 @@ class MoreModuleVC: UIViewController, MoreModuleDisplayLogic {
         viewController.interactor = interactor
         interactor.presenter = presenter
         presenter.viewController = viewController
-
+        
     }
-
-   
+    
+    
     // MARK: View lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //self.doSomething()
         tableView.separatorInset = UIEdgeInsets(top: 0, left: UIScreen.main.bounds.width, bottom: 0, right: 0)
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.isHidden = true
@@ -77,35 +77,35 @@ class MoreModuleVC: UIViewController, MoreModuleDisplayLogic {
         self.navigationController?.addCustomBackButton(title: "")
     }
     // MARK: OpenLoginWindow
-//    func openLoginWindow() {
-//
-//        let vc = DoLoginPopUpVC.instantiate(fromAppStoryboard: .Location)
-//        vc.delegate = self
-//        self.view.alpha = screenPopUpAlpha
-//        self.appDelegate.window?.rootViewController!.present(vc, animated: true, completion: nil)
-//        vc.onDoneBlock = { [unowned self] result in
-//            // Do something
-//            if(result) {} else {}
-//            self.view.alpha = 1.0
-//        }
-//    }
+    //    func openLoginWindow() {
+    //
+    //        let vc = DoLoginPopUpVC.instantiate(fromAppStoryboard: .Location)
+    //        vc.delegate = self
+    //        self.view.alpha = screenPopUpAlpha
+    //        self.appDelegate.window?.rootViewController!.present(vc, animated: true, completion: nil)
+    //        vc.onDoneBlock = { [unowned self] result in
+    //            // Do something
+    //            if(result) {} else {}
+    //            self.view.alpha = 1.0
+    //        }
+    //    }
 }
 
 
 extension MoreModuleVC: UITableViewDelegate, UITableViewDataSource {
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return profileDashboardIdentifiers.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         let identifier = profileDashboardIdentifiers[indexPath.row]
-
+        
         var cell = UITableViewCell()
         if identifier == .notifications{
             guard let notificationCell: NotificationCell = tableView.dequeueReusableCell(withIdentifier: ProfileCellIdentifiers.notifications.rawValue, for: indexPath) as? NotificationCell else {
@@ -123,29 +123,29 @@ extension MoreModuleVC: UITableViewDelegate, UITableViewDataSource {
             cell.selectionStyle = .none
             cell.separatorInset = UIEdgeInsets(top: 0, left: UIScreen.main.bounds.width, bottom: 0, right: 0)
         }
-
+        
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let identifier = profileDashboardIdentifiers[indexPath.row]
         print("Selection: \(identifier.rawValue)")
-
+        
         switch identifier {
-
+            
         case .myProfile:
             let vc = MyProfileVC.instantiate(fromAppStoryboard: .More)
             self.navigationController?.pushViewController(vc, animated: true)
-
-      
+            
+            
         case .notifications :
             let vc = NotificationsVC.instantiate(fromAppStoryboard: .More)
             self.navigationController?.pushViewController(vc, animated: true)
             
         case .logout:break
-//            let navigationC = LoginNavigtionController.instantiate(fromAppStoryboard: .Login)
-//            appDelegate.window?.rootViewController = navigationC
-//            self.signOutUserFromApp()
+            //            let navigationC = LoginNavigtionController.instantiate(fromAppStoryboard: .Login)
+            //            appDelegate.window?.rootViewController = navigationC
+            //            self.signOutUserFromApp()
             
         case .employees:
             let vc = EmployeeListingVC.instantiate(fromAppStoryboard: .More)
@@ -159,7 +159,7 @@ extension MoreModuleVC: UITableViewDelegate, UITableViewDataSource {
             
         }
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 55
     }
@@ -177,12 +177,12 @@ extension MoreModuleVC: UITableViewDelegate, UITableViewDataSource {
 
 // MARK: Call Webservice
 extension MoreModuleVC {
-
+    
     func doSomething() {
         let request = MoreModule.Something.Request(name: "TestData", salary: "100000", age: "10")
         interactor?.doPostRequest(request: request, method: HTTPMethod.post)
     }
-
+    
     func displaySuccess<T: Decodable>(viewModel: T) {
         let obj = viewModel as? MoreModule.Something.Response
         DispatchQueue.main.async {
@@ -194,12 +194,12 @@ extension MoreModuleVC {
             self.showAlert(alertTitle: alertTitle, alertMessage: errorMessage ?? "")
         }
     }
-
+    
     func displaySuccess<T: Decodable>(responseSuccess: [T]) {
         DispatchQueue.main.async {
             var obj = responseSuccess as? [MoreModule.Something.Response]
             print("Get API Response -- \n \(obj)")
         }
     }
-
+    
 }
